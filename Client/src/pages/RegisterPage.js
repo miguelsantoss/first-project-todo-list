ed8059fb09dc1abff84a92356cd56a877984aae2
Miguel Santos <miguel.sl233@gmail.com>
2016-07-16 14:28:37 +0100

    Register route (files were missing)

---
+++ Client/src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class RegisterPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>RegisterPage</h1>
+      </div>
+    );
+  }
+}
