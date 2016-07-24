89b82378a240f2e9372738ec9487c589748eb40d
conblem <2tz_323ojipy1g49@byom.de>
2016-07-25 00:15:28 +0200

    Added csrf_token meta tag for future authentification

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta id="token" name="token" value="{ csrf_token() }">
     <title>Todo App by Team Red Iguanas</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/flatly/bootstrap.min.css" rel="stylesheet">
   </head>
